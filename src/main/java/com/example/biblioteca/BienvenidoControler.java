8f15fa0dac524bff8b6bf2bea47e3c3a59a23fc1
joelmr26 <luh12152@educastur.es>
2025-10-20 20:47:31 +0200

    Implementacion en bienvenida controler

--- src/main/java/com/example/biblioteca/BienvenidoControler.java
+++ src/main/java/com/example/biblioteca/BienvenidoControler.java
@@ -1,9 +1,18 @@
 package com.example.biblioteca;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class BienvenidoControler {
-    public static void main(String[] args) {
-        System.out.println("Bienvenido momnte naranco de nuevo");
-        String release = "Bienvenido a la version oficial";
-    }
+        @GetMapping
+        public String bienvenido(){
+            String release = "Bienvenido a la version oficial";
+            return "Bienvenido momnte naranco de nuevo";
+        }
+
+
 
 }
