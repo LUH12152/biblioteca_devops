f48c4f79123f5f09c2c1e97fc466a560b9ed99ba
joelmr26 <luh12152@educastur.es>
2025-10-24 20:53:29 +0200

    nueva version

---
+++ src/main/java/com/example/biblioteca/entity/Libro.java
@@ -0,0 +1,28 @@
+package com.example.biblioteca.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Table;
+
+@Data //Evitar definir getters y setters
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="libro")
+public class Libro {
+    @Id
+    @GeneratedValue(stragegy =GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column
+    private String autor;
+}
