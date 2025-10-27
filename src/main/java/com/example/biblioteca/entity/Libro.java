0eb5b2c1ccd20242dacabd4850afa761325c4563
joelmr26 <luh12152@educastur.es>
2025-10-27 19:16:39 +0100

    Arreglados fallos clase Libro

--- src/main/java/com/example/biblioteca/entity/Libro.java
+++ src/main/java/com/example/biblioteca/entity/Libro.java
@@ -1,14 +1,11 @@
 package com.example.biblioteca.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Table;
+
 
 @Data //Evitar definir getters y setters
 @Entity
@@ -17,7 +14,7 @@ import org.hibernate.annotations.Table;
 @Table(name="libro")
 public class Libro {
     @Id
-    @GeneratedValue(stragegy =GenerationType.IDENTITY)
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
